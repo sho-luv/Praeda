d9b0b295168934bd9a48a4bbcd2e19e8f50b1528
sho-luv <sho.luv@gmail.com>
2020-07-21 11:18:13 -0500

    Create Install_modules.sh
    install script to install Perl modules

---
+++ Install_modules.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+cpan LWP::Simple
+cpan LWP::UserAgent
+cpan HTML::TagParser
+cpan URI::Fetch
+cpan HTTP::Cookies
+cpan IO::Socket
+cpan HTML::TableExtract
+cpan Getopt::Std
+cpan Net::SSL
+cpan Net::SNMP
+cpan NetAddr::IP
+apt-get install liburi-fetch-perl
